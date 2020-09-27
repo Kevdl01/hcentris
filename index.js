4686e421bbff9b6b6da17c08844f44cbd05785a0
Mark Abdallah <abdallah.marco@gmail.com>
2020-09-27 14:46:02 -0400

    concatenate to one json file possible

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fetchListing = require('./src/fetchlisting') 
+const { concatenateJSON } = require('./src/utils')
 
 const baseUrl = 'https://www.centris.ca'
 const url = `${baseUrl}/en/houses~for-sale~montreal-island?uc=1&view=Thumbnail`
 
 fetchListing(url, baseUrl) 
+
+concatenateJSON('./data')
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,29 @@
 const fs = require('fs')
+const path = require('path')
 
 exports.createJSON = function(dir, json) {
     fs.writeFile(dir, JSON.stringify(json), function(err) {
         if (err) throw err;
+    })
+}
+
+const createDir = function(dir) {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+}
+
+exports.concatenateJSON = function(dir) {
+    createDir(path.resolve(process.env.NODE_PATH) + '/dist')
+    let content = []
+    const files = fs.readdirSync(dir)
+        .filter(name => path.extname(name) === '.json')
+        .map(name => path.join(dir, name))
+    
+    files.forEach(file => {
+        fs.readFile(`${file}`, 'utf-8', function(err, data) {
+            content.push(data)
+            fs.writeFileSync(path.resolve(process.env.NODE_PATH) + '/dist/final.json', JSON.stringify(content));
+        })
     })
 }
