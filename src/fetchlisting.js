d26c770b4e34455c375b546d72f47a3b15c9560f
Mark Abdallah <abdallah.marco@gmail.com>
2020-10-02 07:44:09 -0400

    browser closed properly after fetching listing

--- src/fetchlisting.js
+++ src/fetchlisting.js
@@ -13,10 +13,8 @@ async function boot() {
       const page = await browser.newPage()
       await page.goto('https://www.centris.ca/en/houses~for-sale~montreal-island?uc=1&view=Thumbnail', { waitUntil: 'domcontentloaded' })
       const render = await page.content()
-      
-      await browser.close()
 
-      return { render, page }
+      return { render, page, browser }
   } catch(e) {
       console.log(e)
   }
@@ -30,6 +28,7 @@ const fetchListing = function(url, baseUrl) {
   Promise.resolve(start()).then(function(html) {
     const page = html['render']
     const dom = html['page']
+    const browser = html['browser']
 
     const properties = []
     const links = $('#divMainResult > .thumbnailItem > .shell > a.a-more-detail', page)
@@ -49,6 +48,8 @@ const fetchListing = function(url, baseUrl) {
       console.log(uri)
       // detailinfo(`${baseUrl}${uri}`)
     })
+
+    Promise.resolve(browser.close()).then(() => console.log('browser closed'))
   }, function(err) {
     console.log(err)
   })
